7b66d3f18fbaef051ad3a3df08401d330a7baeaf
dizinnes <dizinnes228@gmail.com>
2021-07-13 21:25:05 +0600

    refactor(javascript): Add best practice solution

--- javascript/Friend or Foe/script.js
+++ javascript/Friend or Foe/script.js
@@ -1,4 +1,4 @@
-const friend = (friends) => {
+const friend2 = (friends) => {
   new_friends = [];
   for (let friend_element of friends) {
       if (friend_element.length == 4) {
@@ -8,6 +8,10 @@ const friend = (friends) => {
   return new_friends;
 }
 
+
+// best practice
+
+const friend = (friends) => friends.filter(x => x.length == 4)
 
 console.log(friend(["Ryan", "Kieran", "Mark"]))
 
