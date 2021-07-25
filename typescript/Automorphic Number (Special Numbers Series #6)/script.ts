5b71702c6a4a813375da6fc919ede4cd70c6e92a
dizinnes <dizinnes228@gmail.com>
2021-07-25 18:24:57 +0600

    refactor(javascript): Update `Automorphic Number (Special Numbers Series #6)` task

--- typescript/Automorphic Number (Special Numbers Series #6)/script.ts
+++ typescript/Automorphic Number (Special Numbers Series #6)/script.ts
@@ -1,5 +1,4 @@
 export const automorphic = (n: number): string => {
-  const numberLength = n.toString().length;
   const sqrt = n * n;
   return sqrt.toString().endsWith(n.toString()) ? 'Automorphic' : 'Not!!'
 }
